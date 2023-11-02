1c22afb1e3fb36a46ce0d270053063ec4f4e41cd
Facundo <137770302+Facundo1984@users.noreply.github.com>
2023-11-02 15:28:30 +0000

    First commit

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
-from utils import db_connect
-engine = db_connect()
+!pip install missingno lazypredict scikit-optimize statsmodels Jinja2
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import statsmodels.api as sm
 
-# your code here
